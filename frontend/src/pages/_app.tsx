1c78d70fc16a3fc147f1d13c9f975b024623f985
Yosuke Mukasa <26806017+mks1412@users.noreply.github.com>
2021-04-29 13:13:10 +0900

    fix: html title tag

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Head from 'next/head'
 
 import '@/styles/global.scss'
-import { config } from '@@/site.config'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
@@ -16,11 +15,6 @@ const App: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <link rel="icon shortcut" type="image/png" href={`${config.siteRoot}/logo.png`} />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
-        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <ChakraProvider>
